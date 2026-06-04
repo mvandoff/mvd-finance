c90d429da1a1dfccaed240bdc8e17b04dbd47fff
Matthew Van Doff <mvandoff@gmail.com>
2026-06-03 21:13:45 -0400

    Format

--- api/src/Api/Program.cs
+++ api/src/Api/Program.cs
@@ -2,47 +2,41 @@ using Api.Auth;
 using Api.Data;
 using Api.Data.Users;
 using Api.Startup;
-
 using Dapper;
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-
 using Npgsql;
-
 using Scalar.AspNetCore;
+
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    )
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(
-    options =>
+builder
+    .Services.AddIdentity<IdentityUser, IdentityRole>(options =>
     {
         options.User.RequireUniqueEmail = true;
-
         options.Password.RequiredLength = 1;
         options.Password.RequiredUniqueChars = 0;
         options.Password.RequireDigit = false;
         options.Password.RequireLowercase = false;
         options.Password.RequireNonAlphanumeric = false;
         options.Password.RequireUppercase = false;
-    }
-)
+    })
     .AddEntityFrameworkStores<AuthDbContext>();
 
-
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+string connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' is missing.");
 builder.Services.AddSingleton(NpgsqlDataSource.Create(connectionString));
 
@@ -60,6 +54,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
+// app.MapIdentityApi
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
